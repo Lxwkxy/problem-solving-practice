7e4b067fab323ea2ab92de0a89ed5b5a6227418b
Phattarapol Deejad <paingaikrab1234@gmail.com>
2026-05-30 00:28:30 +0700

    Add Minimum Element After Replacing With Digit Sum

---
+++ LeetCode/array/minimum_element _after _replacement _with_digit_sum.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        vector<int> sum(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            while(nums[i] > 0) {
+                sum[i] += nums[i] % 10; 
+                nums[i] /= 10;
+            }
+        }
+        int min = sum[0];
+        for (int i = 1; i < sum.size(); i++) {
+            if (sum[i] < min) {
+                min = sum[i];
+            }
+        }
+        return min;
+    }
+};

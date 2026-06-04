33104bd954fb6639bc7af80b9d56ddc9df2b3e15
Phattarapol Deejad <paingaikrab1234@gmail.com>
2026-06-04 19:39:21 +0700

    Add Matrix Diagonal Sum

---
+++ LeetCode/array/matrix_diagonal_sum.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum = 0, n = mat.size();
+        
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i];
+            sum += mat[i][n - 1 - i];
+        }
+ 
+        if (n % 2 != 0) {
+            sum -= mat[n / 2][n / 2];
+        }
+        
+        return sum;
+    }
+};

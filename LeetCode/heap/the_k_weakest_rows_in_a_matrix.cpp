30bc848945437fdb01a1d0c474beec5019f6eb8c
Phattarapol Deejad <paingaikrab1234@gmail.com>
2026-07-03 23:45:49 +0700

    Add The K Weakest Rows in a Matrix

---
+++ LeetCode/heap/the_k_weakest_rows_in_a_matrix.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        vector<int> sum(m);
+
+        for (int i = 0; i < m; i++) {
+            int n = mat[i].size();
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) break;
+                sum[i] += mat[i][j];
+            }
+        }
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+
+        for (int i = 0; i < m; i++) {
+            minHeap.push(make_pair(sum[i], i));
+        }
+        
+        vector<int> ans(k);
+
+        for (int i = 0; i < k; i++) {
+            ans[i] = minHeap.top().second;
+            minHeap.pop();
+        }
+        
+        return ans;
+    }
+};

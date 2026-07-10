eded699708d48d2f0bbd64f70bb3bd2170f1b8ce
Phattarapol Deejad <paingaikrab1234@gmail.com>
2026-07-10 18:46:35 +0700

    Add Maximum Average Subarray I

---
+++ LeetCode/array/maximum_average_subarray_i.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        double maxSum = sum;
+
+        for (int i = k; i < nums.size(); i++) {
+            sum += nums[i];
+            sum -= nums[i - k];
+            maxSum = max(maxSum, sum);
+        }
+
+        return maxSum / k;
+    }
+};

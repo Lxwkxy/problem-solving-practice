95ae9a61af16306f70ed772e1eff7061fd642a2e
Phattarapol Deejad <paingaikrab1234@gmail.com>
2026-07-16 21:53:42 +0700

    Add Kth Largest Element in an Array

---
+++ LeetCode/heap/kth_largest_element_in_an_array.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
+        return nums[k - 1];
+    }
+};

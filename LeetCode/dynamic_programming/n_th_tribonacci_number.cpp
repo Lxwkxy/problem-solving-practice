47a3b5cc7a70dec716d93ece9add7fa9e648f330
Phattarapol Deejad <paingaikrab1234@gmail.com>
2026-07-17 18:38:25 +0700

    Add N-th Tribonacci Number

---
+++ LeetCode/dynamic_programming/n_th_tribonacci_number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n <= 1) return n;
+        if (n == 2) return 1;
+        
+        int t, t1 = 0, t2 = 1, t3 = 1;
+
+        for (int i = 3; i <= n; i++) {
+            t = t1 + t2 + t3;
+            t1 = t2;
+            t2 = t3;
+            t3 = t;
+        }
+
+        return t;
+    }
+};
